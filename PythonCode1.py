7acf67b02eafd971e1764cd9103aebe525c4c59a
daisymani <daisymani.qa@gmail.com>
2025-03-20 15:06:11 +0530

    reusablesAddition

--- PythonCode1.py
+++ PythonCode1.py
@@ -9,3 +9,4 @@ num3 = int(input("Enter the third number: "))
 
 result = add_and_subtract(num1, num2, num3)
 print(f"The result of adding {num1} and {num2}, then subtracting {num3}, is: {result}")
+
